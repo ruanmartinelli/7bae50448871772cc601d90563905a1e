ec92f5b400d7ea801d9083b5b75f1a270a8006eb
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-27 01:29:06 -0300

    add a mini database for storing objects

---
+++ server/helpers/store.js
@@ -0,0 +1,24 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ user: {}, token: {} }).write()
+
+function saveUser(user) {
+  db.set('user', user).write()
+}
+
+function saveToken(token) {
+  db.set('token', token).write()
+}
+
+function getToken() {
+  return db.get('token').value()
+}
+
+function getUser() {
+  return db.get('user').value()
+}
+
+module.exports = { getToken, saveToken, saveUser, getUser }
