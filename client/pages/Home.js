a0473e2c1af8c96cd990cd4e9d8e8ba03874e8e7
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-29 16:53:40 -0300

    list tweets

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -4,16 +4,19 @@ import { get, post } from 'axios'
 class Home extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { twitterData: {} }
+    this.state = { tweets: [], user: {} }
 
     this.getTwitterData = this.getTwitterData.bind(this)
     this.logout = this.logout.bind(this)
   }
 
   getTwitterData() {
-    return post('/connect')
-      .then(res => res.data)
-      .then(data => this.setState({ twitterData: data }))
+    const userDataPromise = post('/connect').then(res => res.data)
+    const tweetDataPromise = get('/tweets').then(res => res.data)
+
+    Promise.all([userDataPromise, tweetDataPromise]).then(([user, tweets]) => {
+      this.setState({ tweets, user })
+    })
   }
 
   logout() {
@@ -26,11 +29,17 @@ class Home extends React.Component {
   }
 
   render() {
-    const {
-      name,
-      screen_name: username,
-      profile_image_url
-    } = this.state.twitterData
+    const { name, screen_name: username, profile_image_url } = this.state.user
+    const { tweets } = this.state
+
+    const tweetsList = tweets.map((tweet, index) => (
+      <div key={index}>
+        <blockquote>
+          <p>{tweet.text}</p>
+          <em>- {tweet.from_user}</em>
+        </blockquote>
+      </div>
+    ))
 
     return (
       <div>
@@ -40,9 +49,11 @@ class Home extends React.Component {
         <img src={profile_image_url} alt="" />
         <h3>{name}</h3>
         <h3>{username}</h3>
+        <div>{tweetsList}</div>
       </div>
     )
   }
 }
 
+// {tweets.map((tweet, index) => <li key={tweet.index}>{tweet.text}</li>)}
 export default Home
