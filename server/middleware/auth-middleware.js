81e99ef9adc1f59a4a28de6213f8d1181a8fa2ac
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-29 19:20:10 -0300

    add null check

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -3,7 +3,7 @@ const store = require('../helpers/store')
 module.exports = (req, res, next) => {
   const { oauthAccessToken, oauthAccessTokenSecret } = store.getToken()
 
-  if(process.env.NODE_ENV.toLowerCase() === 'test') return next()
+  if(process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'test') return next()
 
   if (!oauthAccessToken || !oauthAccessTokenSecret) {
     return res
