f38ff99e359f996c9f83e95ea6665563d1bc9aa9
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-29 16:00:23 -0300

    create /tweets endpoint

--- server/controller/twitter-controller.js
+++ server/controller/twitter-controller.js
@@ -2,16 +2,35 @@ const catchErrrors = require('../helpers/catch-errors')
 const oauthPromise = require('../helpers/oauth-promise')()
 const store = require('../helpers/store')
 
-function init (app) {
+function init(app) {
   app.post('/connect', catchErrrors(getTwitterData))
+  app.get('/tweets', catchErrrors(getUserTweets))
 }
 
-async function getTwitterData (req, res, next) {
+async function getTwitterData(req, res, next) {
   const { screen_name: screenName } = store.getUser()
   const url = `https://api.twitter.com/1.1/users/show.json?screen_name=${screenName}`
   const result = await oauthPromise.get(url)
 
   res.send(result)
+}
+
+async function getUserTweets(req, res, next) {
+  const TWEET_COUNT = 100
+  const { screen_name: screenName } = store.getUser()
+  const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${screenName}&count=${TWEET_COUNT}`
+  const result = await oauthPromise.get(url)
+  const tweets = JSON.parse(result)
+
+  const simplifiedTweets = tweets.map(tweet => {
+    return {
+      text: tweet.text,
+      created_at: tweet.created_at,
+      from_user: tweet.user.screen_name
+    }
+  })
+
+  res.send(simplifiedTweets)
 }
 
 module.exports = { init }
