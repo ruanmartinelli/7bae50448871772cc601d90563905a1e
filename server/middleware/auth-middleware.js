5ce70e80805c5b8cceda3e99a8e335a42c6df78a
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-29 19:18:42 -0300

    ignore token check in test env

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -3,6 +3,8 @@ const store = require('../helpers/store')
 module.exports = (req, res, next) => {
   const { oauthAccessToken, oauthAccessTokenSecret } = store.getToken()
 
+  if(process.env.NODE_ENV.toLowerCase() === 'test') return next()
+
   if (!oauthAccessToken || !oauthAccessTokenSecret) {
     return res
       .send({ success: false, message: 'Please sign in firs' })
